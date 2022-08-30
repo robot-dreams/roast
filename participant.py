9dedde943ce226d797851d8d147458016edf8ea1
Elliott Jin <elliott.jin@gmail.com>
2022-08-30 13:59:18 -0700

    Precompute nonces dynamically

--- participant.py
+++ participant.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process, Queue
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, IPPROTO_TCP, TCP_NODELAY
 
 import logging
@@ -7,35 +8,26 @@ import time
 from roast import pre_round, sign_round
 from transport import send_obj, recv_obj
 
-class NonceCache:
-    def precompute(self, k):
-        self.cached = []
-        for i in range(k):
-            self.cached.append(pre_round())
-
-    def get(self):
-        if len(self.cached) == 0:
-            raise Exception('no more precomputed nonces')
-        return self.cached.pop()
+MAX_NONCE_QUEUE = 16
 
 class Participant:
-    def __init__(self, X, i, sk_i, nonce_cache):
+    def __init__(self, X, i, sk_i, nonce_queue):
         self.X = X
         self.i = i
         self.sk_i = sk_i
-        self.nonce_cache = nonce_cache
-        self.spre_i, self.pre_i = nonce_cache.get()
+        self.nonce_queue = nonce_queue
+        self.spre_i, self.pre_i = nonce_queue.get()
 
     def sign_round(self, msg, T, pre):
         s_i = sign_round(self.X, msg, T, pre, self.i, self.sk_i, self.spre_i)
-        self.spre_i, self.pre_i = self.nonce_cache.get()
+        self.spre_i, self.pre_i = self.nonce_queue.get()
         return s_i, self.pre_i
 
-def handle_requests(connection, nonce_cache):
+def handle_requests(connection, nonce_queue):
     X, i, sk_i = recv_obj(connection)
     logging.debug(f'Participant {i}: Received initialization data')
 
-    participant = Participant(X, i, sk_i, nonce_cache)
+    participant = Participant(X, i, sk_i, nonce_queue)
     send_obj(connection, (i, None, participant.pre_i))
     logging.debug(f'Participant {i}: Sent initial pre_i value')
 
@@ -55,17 +47,19 @@ def handle_requests(connection, nonce_cache):
             send_obj(connection, (i, s_i, pre_i))
             logging.info(f'Participant {i}: Sent sign_round response and next pre_i value in {elapsed:.4f} seconds')
 
+def compute_nonce_loop(nonce_queue):
+    while True:
+        nonce_queue.put(pre_round())
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    if len(sys.argv) != 3:
-        print(f'usage: {sys.argv[0]} <port> <num_precomputed_nonces>')
+    if len(sys.argv) != 2:
+        print(f'usage: {sys.argv[0]} <port>')
         sys.exit(1)
 
-    nonce_cache = NonceCache()
-    k = int(sys.argv[2])
-    nonce_cache.precompute(k)
-    logging.info(f'Done precomputing {k} nonces')
+    nonce_queue = Queue(MAX_NONCE_QUEUE)
+    Process(target=compute_nonce_loop, args=[nonce_queue], daemon=True).start()
 
     port = int(sys.argv[1])
     addr = ('0.0.0.0', port)
@@ -83,6 +77,6 @@ if __name__ == '__main__':
         logging.debug('Accepted connection from {src}')
 
         try:
-            handle_requests(connection, nonce_cache)
+            handle_requests(connection, nonce_queue)
         except ConnectionResetError as e:
             print(e)
